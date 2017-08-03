d98e9249a3f4be7842a855134dfccead96771456
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-02 22:32:54 -0400

    madechangesgotprofileandavailableforhire

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const url = 'mongodb://localhost:27017/robots';
 
 var findBrokeRobots = function(db, callback) {
   var notForHire = db.collection('robots');
-
-  notForHire.find({"job" : null}).toArray(function(err) {
+  notForHire.find({"job" : null}).toArray(function(err, result) {
     console.log("found", result.length, "users")
     callback(result);
   });
@@ -20,10 +19,22 @@ var findAllRobots = function(db, callback) {
     callback(result);
   });
 }
+var findEmployedRobots = function(db, callback) {
+  var notForHire = db.collection('robots');
+  notForHire.find({"company" : 'string' }).toArray(function(err, result) {
+    console.log("found", result.length, "users");
+    callback(result);
+  });
+}
+
 MongoClient.connect(url, function(err, db) {
   console.log('error?' , err);
   console.log("We have connected");
-  db.close();
+
+
+  findBrokeRobots(db, function() {
+    console.log('Search done. Peace.');
+    db.close();
   });
 });
 app.engine('mustache', mustacheExpress());
@@ -34,10 +45,10 @@ app.set('views', __dirname + '/views');
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello there It is now' + new Date()) + `<a href="http://localhost:3000/indexrobots">robot?</a>`
+  res.send('Hello there! Do you need ' + `<a href="http://localhost:3000/indexrobots">Robots?</a>`);
 });
 
-app.get("/indexrobots", function(req, res) {
+app.get('/indexrobots', function(req, res) {
   MongoClient.connect(url, function(err, db) {
    findAllRobots(db, function(result) {
      res.render('allRobos', {users : result} );
@@ -45,9 +56,17 @@ app.get("/indexrobots", function(req, res) {
  });
 });
 
-app.get('/unemployed', function(req, res) {
+app.get('/brokerobots', function(req, res) {
   MongoClient.connect(url, function(err, db) {
    findBrokeRobots(db, function(result) {
+     res.render('allRobos', {users : result} );
+   });
+ });
+});
+
+app.get('/employedrobots', function(req, res) {
+  MongoClient.connect(url, function(err, db) {
+   findEmployedRobots(db, function(result) {
      res.render('allRobos', {users : result} );
    });
  });
