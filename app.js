b61020579af54f9b4cc90403224f965bd5634e52
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-03 22:02:11 -0400

    somework

--- app.js
+++ app.js
@@ -1,9 +1,69 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const app = express();
 const MongoClient = require('mongodb').MongoClient;
-
 const url = 'mongodb://localhost:27017/robots';
+const mongoose = require('mongoose');
+const expressValidator = require('express-validator');
+const passport = require('passport');
+const fs = require('fs');
+const path = require('path');
+const model = require("./model");
+const user = model.User;
+
+const app = express();
+
+mongoose.connect('mongodb://localhost/test');
+
+app.engine('mustache', mustacheExpress());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+app.use(express.static('public'));
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.authenticate(username, password, function(err, user){
+      if (err){
+        return done(err)
+      }
+      if (user){
+        return done(null, user)
+      }else{
+        return done(null, false, {
+          message:"No user with that username and password is found."
+        })
+      }
+    })
+  }));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(session({
+  secret: 'Master Rahool',
+  resave: false,
+  saveUninitialized: false,
+  store: new(require('express-sessions'))({
+    storage: 'mongodb'
+  })
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function (req, res, next){
+  res.locals.user = req.user;
+  next();
+})
 
 var findBrokeRobots = function(db, callback) {
   var notForHire = db.collection('robots');
@@ -32,22 +92,80 @@ MongoClient.connect(url, function(err, db) {
   console.log('error?' , err);
   console.log("We have connected");
 
-
   findBrokeRobots(db, function() {
     console.log('Search done. Peace.');
     db.close();
   });
 });
-app.engine('mustache', mustacheExpress());
 
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
-
-app.use(express.static('public'));
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+})
 
 app.get('/', function (req, res) {
-  res.send('Hello there! Do you need ' + `<a href="http://localhost:3000/indexrobots">Robots?</a>`);
+     res.render("allRobos");
+   })
+
+app.post('/login/', passport.authenticate('local', {
+  sucessRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+app.get('/register/', function(req, res) {
+       res.render('register');
+   });
+
+  app.post('register/', function(req, res) {
+    req.checkBody('username', 'Username must be alphanimeric').isAlphanumeric();
+    req.checkBody('username', 'Username is required').notEmpty();
+    req.checkBody('password', 'Password is required').notEmpty();
+
+    req.getValidationResult()
+    .then(function(result){
+      if (!result.isEmpty()) {
+        return res.render("register", {
+        username: req.body.username,
+        errors: result.mapped()
+      });
+    }
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password
+    })
+
+    const error = user.validateSync();
+    if (error) {
+      return res.render("register", {
+        errors: normalizeMongooseErrors(error.errors)
+      })
+    }
+
+    user.save(function(err) {
+      if (err) {
+        return res.render("register", {
+          messages: {
+            error: ["That username is taken."]
+          }
+        })
+      }
+      return res.redirect('/');
+    })
+  })
 });
+
+app.get('/logout/', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+const requireLogin = function (req, res, next) {
+  if (req.user) {
+    next()
+  } else {
+    res.redirect('/login/');
+  }
+}
 
 app.get('/indexrobots', function(req, res) {
   MongoClient.connect(url, function(err, db) {
---
+++ model.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    unique: true,
+    lowercase: true,
+    required: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  }
+});
+
+userSchema.virtual('password')
+  .get(function() {
+    return null
+  })
+  .set(function(value) {
+    const hash = bcrypt.hash(value, 8);
+    this.passwordHash = hash;
+  })
+
+userSchema.methods.authenticate = function(password) {
+  return bcrypt.compareSync(password, this.passwordHash);
+}
+
+userSchema.statics.authenticate = function(username, password, done) {
+  this.foundOne({
+    username: username
+  }, function(err, user) {
+    if (err) {
+      done(err, false)
+    } else if (user && user.authenticate(password)) {
+      done(null, user)
+    } else {
+      done(null, false)
+    }
+  })
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = {
+  User: user
+};
